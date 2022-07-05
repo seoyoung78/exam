83adc66fdb2550fab5435966a6f76cee6146478a
seoyoung78 <tjdud0708@naver.com>
2022-07-05 21:16:08 +0900

    final

---
+++ src/ch06/PersonExample.java
@@ -0,0 +1,11 @@
+package ch06;
+
+public class PersonExample {
+    public static void main(String[] args) {
+        Person p1 = new Person("123456-1234567", "계백");
+
+        System.out.println(p1.nation);
+        System.out.println(p1.ssn);
+        System.out.println(p1.name);
+    }
+}
